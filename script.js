2df3a13d0f7fcc1783c714bb575ef64c45083e88
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2023-06-03 19:17:34 -0300

    Create grid of pixels

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const grid = document.querySelector('.grid');
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    for (let j = 0; j < 16; j++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        row.appendChild(pixel);
+    }
+    row.classList.add('row');
+    grid.appendChild(row);
+}
