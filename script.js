040f9de85188e68619b9063f9f180142b135ec64
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2023-06-03 20:30:10 -0300

    Implement resize grid button

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
 'use strict';
 
-const grid = document.querySelector('.grid');
+const resizeButton = document.querySelector('.resizeButton');
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    for (let j = 0; j < 16; j++) {
-        const pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        row.appendChild(pixel);
+function createGrid(size) {
+    let result = document.createElement('div');
+    result.classList.add('grid');
+    
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        for (let j = 0; j < size; j++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            row.appendChild(pixel);
+        }
+        row.classList.add('row');
+        result.appendChild(row);
     }
-    row.classList.add('row');
-    grid.appendChild(row);
+
+    result.addEventListener('mouseover', e => {
+        e.target.style.background = 'black';
+        e.stopPropagation();
+    })
+    
+    return result;
 }
 
-grid.addEventListener('mouseover', e => {
-    e.target.style.background = 'black';
-    e.stopPropagation();
+resizeButton.addEventListener('click', e => {
+    const size = Number.parseInt(prompt('Set the number of pixels per side (max: 100)'));
+
+    if (!size) {
+        alert("Invalid input!");
+        return;
+    }
+
+    if (size < 0) {
+        alert("Must be a positive integer!");
+        return;
+    }
+
+    if (size > 100) {
+        alert("Number of pixels per side can't exceed 100!");
+        return;
+    }
+
+    const oldGrid = document.querySelector('.grid');
+    oldGrid.parentElement.replaceChild(createGrid(size), oldGrid);
 })
+
+function main() {
+    const grid = document.querySelector('.grid');
+    grid.parentElement.replaceChild(createGrid(16), grid);
+}
+
+main();
