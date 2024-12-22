e17672f351feb17e008215efa222e756b262b872
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2024-12-22 20:23:03 -0300

    Finish grid creation and drawing mechanic

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
 "use strict";
 
+
+const GRID_SIZE = 16;
+
+const grid = document.querySelector(".grid");
+
+
+createGrid(GRID_SIZE);
+
+
+function createGrid(size) {
+    const row = createRow(size);
+
+    clearGrid();
+
+    for (let i = 0; i < size; i++)
+        grid.appendChild(row.cloneNode(true));
+}
+
+function createRow(size) {
+    const row = document.createElement("div");
+    const cell = document.createElement("div");
+    row.classList.add("row");
+    cell.classList.add("cell");
+
+    for (let i = 0; i < size; i++)
+        row.appendChild(cell.cloneNode());
+
+    return row;
+}
+
+function clearGrid() {
+    while (grid.firstChild) {
+        grid.firstChild.remove();
+    }
+}
+
+grid.addEventListener("mouseover", event => {
+    if (event.target.classList.contains("cell")) {
+        event.target.classList.add("painted");
+    }
+});
